baa391968e3eeac64e30d231057e0cf5fda041d4
ROHAN VINOD CHAVHAN <69052636+Rohan3214@users.noreply.github.com>
2022-11-21 23:14:08 +0530

    Assignment 1
    This a Pattern Programming 1st Assignment, where I have create Patter of my initial name.

---
+++ Rohan.java
@@ -0,0 +1,93 @@
+package com.Ineuron.in;
+
+public class Rohan 
+{
+	public static void main(String args[])
+	{
+		int n = 10;
+		for(int i = 0; i <= n; i++)
+		{
+			for(int j =0; j<= n; j++)
+			{
+				 if((i >= 0 && j==0 && i <= n-1) ||
+					(i ==0 && j <= (3*n)/4) || 
+					(j == (3*n)/4 && i <= (n-1)/2 && i >=0) || 
+					(i == (n-1)/2 &&j <= (3*n)/4 ) || 
+					(i == (2*n)/4 && j == (n)/4) || 
+					(i == (3*n)/4 && j == (n-1)/2) || 
+					(i ==(n-1) && j ==(3*n)/4)
+						 )
+				 {
+					 System.out.print("*");
+				 }
+				 else
+				 {
+					 System.out.print(" ");
+				 }
+			}
+			
+			for(int j =0; j<= n; j++)
+			{
+				if((i <= 0 && j <= (3*n)/4 ) || 
+					( i > 0 && i <= n-1 && j ==0) || 
+					(i ==(n-1) && j <= (3*n)/4) || 
+					( i >0 && i < (n-1) && j ==(3*n)/4))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j =0; j<= n; j++)
+			{
+				if((i >= 0 && i<= (n-1) && j==0) || 
+						(j > 0 && j < (3*n)/4 && i ==(n-1)/2) || 
+						(i >= 0 && i<= (n-1) && j==(3*n)/4))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j =0; j<= n; j++)
+			{
+				if( ( i == 0 && j <= (3*n)/4) || 
+						( i >= 0 && i <= (n-1) && j ==0) || 
+						(i >= 0 && i <= (n-1) && j == (3*n)/4) || 
+						(i == (n-1)/2 && j > 0 && j < (3*n)/4) 
+						)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j =0; j<= n ; j++)
+			{
+				if( (i >= 0 && i <= (n-1) && j ==0) || 
+						(j == n/4 && i == n/4) || 
+						(i ==(n-1)/2 && j == (n-1)/2 ) ||
+						(i == (3*n)/4 && j == (3*n)/4) || 
+						(i == (n-1) && j == (n-1)) ||
+						(i >= 0 && i <= (n-1) && j == (n-1)))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
